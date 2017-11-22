1f4deb1b1060f768bba828ee68e3a1d071c29ddc
Luc Wollants <luc.wollants@apptite.be>
2017-11-22 10:45:32 +0100

    III-2391 Catching UiTPAS exceptions and mapping to no ticket sales.

--- tests/Event/CommandHandling/Validation/EventHasTicketSalesCommandValidatorTest.php
+++ tests/Event/CommandHandling/Validation/EventHasTicketSalesCommandValidatorTest.php
@@ -2,6 +2,7 @@
 
 namespace CultuurNet\UDB3\UiTPAS\Event\CommandHandling\Validation;
 
+use CultureFeed_HttpException;
 use CultuurNet\UDB3\Event\Commands\UpdateOrganizer;
 use CultuurNet\UDB3\Event\Commands\UpdatePriceInfo;
 use CultuurNet\UDB3\Place\Commands\UpdateOrganizer as UpdatePlaceOrganizer;
@@ -9,6 +10,7 @@ use CultuurNet\UDB3\Place\Commands\UpdatePriceInfo as UpdatePlacePriceInfo;
 use CultuurNet\UDB3\PriceInfo\BasePrice;
 use CultuurNet\UDB3\PriceInfo\Price;
 use CultuurNet\UDB3\PriceInfo\PriceInfo;
+use Psr\Log\LoggerInterface;
 use ValueObjects\Money\Currency;
 
 class EventHasTicketSalesCommandValidatorTest extends \PHPUnit_Framework_TestCase
@@ -19,6 +21,11 @@ class EventHasTicketSalesCommandValidatorTest extends \PHPUnit_Framework_TestCas
     private $uitpas;
 
     /**
+     * @var LoggerInterface|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $logger;
+
+    /**
      * @var EventHasTicketSalesCommandValidator
      */
     private $validator;
@@ -26,7 +33,12 @@ class EventHasTicketSalesCommandValidatorTest extends \PHPUnit_Framework_TestCas
     public function setUp()
     {
         $this->uitpas = $this->createMock(\CultureFeed_Uitpas::class);
-        $this->validator = new EventHasTicketSalesCommandValidator($this->uitpas);
+        $this->logger = $this->createMock(LoggerInterface::class);
+
+        $this->validator = new EventHasTicketSalesCommandValidator(
+            $this->uitpas,
+            $this->logger
+        );
     }
 
     /**
@@ -111,6 +123,29 @@ class EventHasTicketSalesCommandValidatorTest extends \PHPUnit_Framework_TestCas
             ->method('eventHasTicketSales')
             ->with('5e75970e-43d8-481f-88db-9a61dd087cbb')
             ->willReturn(false);
+
+        $this->validator->validate($command);
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_handle_uitpas_exceptions_as_no_ticket_sales()
+    {
+        $command = new UpdateOrganizer(
+            '5e75970e-43d8-481f-88db-9a61dd087cbb',
+            '596c4837-6239-47e3-bf33-2bb11dc6adc7'
+        );
+
+        $this->uitpas->expects($this->once())
+            ->method('eventHasTicketSales')
+            ->with('5e75970e-43d8-481f-88db-9a61dd087cbb')
+            ->willThrowException(new CultureFeed_HttpException('result message', 404));
+
+        $this->logger->expects($this->once())
+            ->method('warning')
+            ->with('Ticket call sales failed with exception message "The reponse for the HTTP request was not 200. result message" and exception code "404".'
+                . ' Assuming no ticket sales for event 5e75970e-43d8-481f-88db-9a61dd087cbb');
 
         $this->validator->validate($command);
     }
